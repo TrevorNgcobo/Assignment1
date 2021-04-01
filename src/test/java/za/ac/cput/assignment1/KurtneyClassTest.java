e883c78800cf0934c9d0e1cf9b0b231bc3700c45
Kurtney <kurtjantjies18@gmail.com>
2021-04-01 13:01:53 +0200

    Final Stages

--- src/test/java/za/ac/cput/assignment1/KurtneyClassTest.java
+++ src/test/java/za/ac/cput/assignment1/KurtneyClassTest.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package za.ac.cput.assignment1;
 
 
@@ -14,7 +10,9 @@ import org.junit.jupiter.api.Timeout;
 
 /**
  *
- * @author Killa-Bee
+ * @author Kurtney Jantjies
+ * Student Number: 218138105
+ * Group 20
  */
 public class KurtneyClassTest {
     
