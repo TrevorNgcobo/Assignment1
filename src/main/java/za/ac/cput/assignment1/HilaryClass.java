0fb64ac7fecbbff722604cd139e09a596232e3c7
Hilary- 16 <220346887@mycput.ac.za>
2021-04-01 19:08:54 +0200

    added some new comments

--- src/main/java/za/ac/cput/assignment1/HilaryClass.java
+++ src/main/java/za/ac/cput/assignment1/HilaryClass.java
@@ -10,6 +10,83 @@ package za.ac.cput.assignment1;
  *
  * @author Killa-Bee
  */
-public class HilaryClass {
+public class HilaryClass
+{
+  private String name;
+    private double salary;
+    private double hoursPerWeek;
+    private int leaveDays;
+  
+   
+     public HilaryClass(String nam,double salar,double hoursPerWee,int leaveDay)
+     {
+        name=nam;
+        salary=salar;
+        hoursPerWeek=hoursPerWee;
+        leaveDays=leaveDay;
+     }
 
+    HilaryClass() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public double getHoursPerWeek() {
+        return hoursPerWeek;
+    }
+
+    public int getLeaveDays() {
+        return leaveDays;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public void setHoursPerWeek(double hoursPerWeek) {
+        this.hoursPerWeek = hoursPerWeek;
+    }
+
+    public void setLeaveDays(int leaveDays) {
+        this.leaveDays = leaveDays;
+    }
+      public void leaveApplication()
+    {
+    System.out.println("Please go to our online website to apply for leave");
+    }
+    public void work()
+    {
+        System.out.println("job description : worker");
+    }
+     public String toString()
+     {
+     String str1=String.format("NAME----%s||SALARY----R%.2f||Hours per week----%.2f||LEAVE DAYS----%d",this.name,this.salary,this.hoursPerWeek,this.leaveDays);
+     return str1;
+     }
+     public double costOfBusiness()
+    {
+    double x=0,dayOfYear=365; 
+    x=getSalary()/dayOfYear;
+    
+    x=x*getLeaveDays();
+    return x;
+    }
+     
+     public static void main(String[] args)
+     {
+        
+    }
 }
+
+
--- src/test/java/za/ac/cput/assignment1/HilaryClassTest.java
+++ src/test/java/za/ac/cput/assignment1/HilaryClassTest.java
@@ -5,42 +5,70 @@
  */
 package za.ac.cput.assignment1;
 
-import org.junit.After;
-import org.junit.AfterClass;
+import java.util.concurrent.TimeUnit;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Timeout;
 
 /**
  *
  * @author Killa-Bee
  */
-public class HilaryClassTest {
-    
-    public HilaryClassTest() {
-    }
-    
-    @BeforeClass
-    public static void setUpClass() {
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
-    
+public class HilaryClassTest 
+{
+    private HilaryClass ITDepartment1;
+    private HilaryClass ITDepartment2;
+    private HilaryClass ITDepartment3;
+    private HilaryClass ITDepartment4;
+   
     @Before
-    public void setUp() {
+    
+    public void setUp() 
+    {
+     ITDepartment1= new HilaryClass();
+     ITDepartment2=ITDepartment3;
+     ITDepartment1= new HilaryClass();
     }
     
-    @After
-    public void tearDown() {
+    @Test
+    public void testEquality()
+    {
+        assertEqual(ITDepartment1, ITDepartment2);
     }
 
     @Test
-    public void testSomeMethod() {
-        // TODO review the generated test code and remove the default call to fail.
+    public void testIdentity()
+    {
+        assertSame(ITDepartment3, ITDepartment2);
         
     }
     
+    @Test
+    public void FailingTest()
+    {
+        int blue1=4,blue2=5;
+        int total = 55;
+        fail("Test has failed on my accord");
+    }
+    
+    @Test
+    @Timeout(value = 1000,unit = TimeUnit.MILLISECONDS)
+    public void TimeoutTest()
+    {
+        while (true);
+    }
+    
+    @Disabled
+    @Test
+    public void testDisabled()
+    {
+        System.out.println("Test has failed hence it needs to be disabled");
+    }
+    
+
+    private void assertEqual(HilaryClass ITDepartment1, HilaryClass ITDepartment2) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }
