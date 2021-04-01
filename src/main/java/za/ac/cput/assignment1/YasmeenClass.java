47c93923a59bc7cce70fe5304839c858ca1edfb7
Yasmeen Nel <yasmeennel@gmail.com>
2021-04-01 02:40:31 +0200

    Yasmeen has written a program in her own class

--- src/main/java/za/ac/cput/assignment1/YasmeenClass.java
+++ src/main/java/za/ac/cput/assignment1/YasmeenClass.java
@@ -8,8 +8,39 @@ package za.ac.cput.assignment1;
 
 /**
  *
- * @author Killa-Bee
+ * @Yasmeen Nel 219250553
  */
 public class YasmeenClass {
 
+    private String age, name, student;
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStudent() {
+        return student;
+    }
+
+    public void setStudent(String student) {
+        this.student = student;
+    }
+
+    @Override
+    public String toString() {
+        return "YasmeenClass{" + "age=" + age + ", name=" + name + ", faculty=" + student + '}';
+    }
+    
 }
